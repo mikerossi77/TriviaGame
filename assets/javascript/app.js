182894c6fa74ad84ef791d7ae1f96f54a65c5321
Michael Rossi <michaelrossi1856@gmail.com>
2019-06-17 17:08:35 -0700

    Added timers.  No overall score

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-
-
     // Questions array
     var questions = [
         {
@@ -28,7 +26,7 @@ $(document).ready(function () {
             explanation: "There are 7 days in the week. They are: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"
         },
         {
-            question: "How months are there in a year?",
+            question: "How many months are there in a year?",
             answers: [
                 "4",
                 "10",
@@ -52,26 +50,48 @@ $(document).ready(function () {
         }
     ]
     let currentQuestionNum = 0;
+    var timePerQuestion = 10;
     $("#gameTimer").hide();
+    var time = timePerQuestion;
+    var timeRemaining;
+    var timeBeforeNextQuestion;
+    var firstQuestion = true;
 
     const startQuestion = function () {
         //Write question and answers to page
         $("#gameMain").empty();
         var currentQuestion = questions[currentQuestionNum].question
         $("#gameMain").append("<h1> " + currentQuestion + "</h1>");
+        //clear show answer
+        clearInterval(timeBeforeNextQuestion);
+        // Timer Counting Down
+        $("#secondsLeft").text(time);
+        time = timePerQuestion;
+        timeRemaining = setTimeout(outOfTime, timePerQuestion*1000);
+        if (firstQuestion) {
+            intervalId = setInterval(count, 1000);
+        }
+        firstQuestion = false;
+        
+
+
         //Loop through answers and write them to page
         questions[currentQuestionNum].answers.forEach(function (answer, i) {
             //$("#gameMain").append("<p> " + answer + "</p>");
             var newAnswer = $("<h2 class = answer value = " + i + ">" + answer + "</h2>").on("click", function () {
-                console.log("answer clicked")
+                // Answer Clicked
+                stopTimer();
+
                 //get their answer
                 theirAnswerNumber = $(this).attr("value");
                 var theAnswer = questions[currentQuestionNum].correctAnswer;
+                
                 // If correct
                 if (theirAnswerNumber === theAnswer) {
                     $("#gameMain").empty();
                     $("#gameMain").append("<h1>That is correct!</h1>");
                 }
+                
                 // If wrong
                 else {
                     $("#gameMain").empty();
@@ -79,18 +99,15 @@ $(document).ready(function () {
                     $("#gameMain").append("<h1>The correct answer is:  " + questions[currentQuestionNum].answers[questions[currentQuestionNum].correctAnswer] + "</h1>");
                     $("#gameMain").append("<h1>" + questions[currentQuestionNum].explanation + "</h1>");
                 }
+                
                 //Show next Question button
                 var nextQuestionButton = $("<button id = 'nextQuestionButton'>Next Question</button>").click(function () {
-                    console.log("next question pressed");
-                    currentQuestionNum++;
-                    if (currentQuestionNum < questions.length) {
-                        startQuestion();
-                    }
-                    else {
-                        console.log("GAME OVER!")
-                    }
+                    stopTimer();
+                    nextQuestion();
                 })
                 $("#gameMain").append(nextQuestionButton);
+
+
             });
             $("#gameMain").append(newAnswer);
             //$("#gameMain").append("<p class = answer value = " + i + ">" + answer + "</p>");
@@ -102,7 +119,37 @@ $(document).ready(function () {
         startQuestion();
     });
 
+    var nextQuestion = function () {
+        stopTimer();
+        currentQuestionNum++;
+        time = timePerQuestion;
+        if (currentQuestionNum < questions.length) {
+            startQuestion();
+        }
+        else {
+            console.log("GAME OVER!")
+        }
+    }
+    var outOfTime = function () {
+        stopTimer();
+        $("#gameMain").empty();
+        $("#gameMain").append("<h1>You are out of time!</h1>");
+        $("#gameMain").append("<h1>The correct answer is:  " + questions[currentQuestionNum].answers[questions[currentQuestionNum].correctAnswer] + "</h1>");
+        $("#gameMain").append("<h1>" + questions[currentQuestionNum].explanation + "</h1>");
+        timeBeforeNextQuestion = setTimeout(nextQuestion, timePerQuestion*1000)
 
-    // .ready ending
+    }
+
+    function count() {
+        time--;
+        $("#secondsLeft").text(time);
+    }
+    function stopTimer() {
+        clearInterval(timeRemaining);
+        clockRunning = false;
+        $("#gameTimer").hide();
+
+    }
+
 });
 
